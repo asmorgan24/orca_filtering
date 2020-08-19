318663f2cd7ab558e321a127645820c27219905c
Andy Morgan <andrew.morgan@yale.edu>
2020-08-19 12:03:36 -0400

    generate three files according to groups

--- generate_inp.py
+++ generate_inp.py
@@ -1,227 +1,77 @@
-# ###################################
-# # Place Substituent at position 3 (H91)
-# ###################################
-#
-# # MUST REMOVE H91
-# # Place H91
-# H_91 = ["H91         2.0061000000     -0.0616400000      4.8591500000"]
-# # Place F:
-# F_91 =  ["F        2.0061000000     -0.0616400000      4.8591500000"]
-# # Place CH3:
-# CH3_91 = [
-# "C        2.3950440000     -0.0546040000      5.0472100000",
-# "H        3.2611800000      0.1057110000      4.3908790000",
-# "H        2.5553310000     -0.9933940000      5.5997270000",
-# "H        2.3760970000      0.7523060000      5.7931150000"
-# ]
-# # Place CF3:
-# CF3_91 = [
-# "C        2.3633320000      0.0116780000      5.0892620000",
-# "F        3.4787960000      0.0385070000      4.3540190000",
-# "F        2.4656460000     -1.0051290000      5.9513320000",
-# "F        2.3391370000      1.1346620000      5.8241610000"
-# ]
-# # Place OCH3:
-# OCH3_91 = [
-# "O        2.2205800000     -0.1248610000      5.0290430000",
-# "C        3.4809380000     -0.0877030000      4.4128240000",
-# "H        4.2303060000     -0.1260930000      5.2102050000",
-# "H        3.6265080000      0.8411530000      3.8336550000",
-# "H        3.6270200000     -0.9469020000      3.7366980000"
-# ]
-# # Place CO2CH3
-# CO2CH3_91 = [
-# "C        2.4463300000     -0.0757000000      4.9586600000",
-# "O        3.3251080000      0.6576720000      4.6019770000",
-# "O        2.7102590000     -0.9565670000      5.9415160000",
-# "C        1.8015590000     -1.7454280000      6.6818110000",
-# "H        2.4341880000     -2.3986010000      7.2938530000",
-# "H        1.1607770000     -2.3709200000      6.0416110000",
-# "H        1.1820830000     -1.1232020000      7.3428960000"
-# ]
-#
-# # Place COCH3:
-# COCH2_91 = [
-# "C        2.4092610000     -0.0315840000      5.0215720000",
-# "O        3.4755620000     -0.1740270000      4.4728940000",
-# "C        2.3201230000      0.2642870000      6.5038550000",
-# "H        3.3002550000      0.6067000000      6.8532320000",
-# "H        2.0463970000     -0.6482130000      7.0547140000",
-# "H        1.5596420000      1.0262990000      6.7134840000"
-# ]
-#
-# # Place CONMe2:
-# CONMe2_91 = [
-# "C        2.4454200000     -0.1329350000      4.9755760000",
-# "O        2.9820420000      0.9041120000      5.3097740000",
-# "N        3.0075250000     -1.3650540000      5.2277170000",
-# "C        4.3071260000     -1.4162740000      5.8629800000",
-# "C        2.5913320000     -2.6127760000      4.6204730000",
-# "H        4.5153750000     -0.4516940000      6.3390210000",
-# "H        5.0999040000     -1.6184210000      5.1206440000",
-# "H        4.3261610000     -2.2091420000      6.6243840000",
-# "H        1.5926980000     -2.5360530000      4.1756930000",
-# "H        2.5942630000     -3.4067470000      5.3812080000",
-# "H        3.2911810000     -2.9198690000      3.8222520000"
-# ]
-#
-# # Place Beg:
-# Beg_91 = [
-# "B        2.4261500000      0.0133370000      5.0882150000",
-# "O        3.6714380000      0.1752940000      4.5458880000",
-# "O        2.4447100000     -0.0168610000      6.4566390000",
-# "C        4.6196290000      0.2519320000      5.5975280000",
-# "C        3.7945620000      0.0501860000      6.8902530000",
-# "H        5.1279230000      1.2287880000      5.5644710000",
-# "H        5.3841810000     -0.5273570000      5.4692080000",
-# "H        4.0606910000     -0.8884920000      7.4006290000",
-# "H        3.9085680000      0.8691850000      7.6144010000"
-# ]
-#
-#
-#
-# H91 = [H_91, F_91,CH3_91, CF3_91, OCH3_91, CO2CH3_91, COCH2_91, CONMe2_91, Beg_91]
-# H91_names = ["H", "F", "CH3", "CF3", "OCH3", "CO2CH3", "COCH2", "CONMe2", "Beg"]
-#
-#
-# ################################################################
-#
-# ###################################
-# # Place Substituent at position 4 (H92)
-# ###################################
-#
-# # MUST REMOVE H92
-#
-# # Place CH3:
-# CH3_92 = [
-# "C       -0.1322240000     -0.5641100000      6.3662620000",
-# "H       -1.1231170000     -0.8794500000      6.7193670000",
-# "H        0.1766360000      0.3312860000      6.9275440000",
-# "H        0.5816370000     -1.3600850000      6.6208840000"
-# ]
-# # Place CF3:
-# CF3_92 = [
-# "C       -0.1347970000     -0.4785270000      6.3764720000",
-# "F       -1.3156550000     -0.9305960000      6.8099430000",
-# "F        0.1141180000      0.6745150000      7.0055140000",
-# "F        0.8022100000     -1.3452050000      6.7863940000"
-# ]
-# # Place OCH3:
-# OCH3_92 = [
-# "O       -0.1002450000     -0.5217520000      6.2134580000",
-# "C       -1.2950540000     -0.8883650000      6.8502100000",
-# "H       -1.0285570000     -1.1611610000      7.8785870000",
-# "H       -1.7587680000     -1.7661380000      6.3659910000",
-# "H       -2.0314800000     -0.0691970000      6.8681530000"
-# ]
-# # Place CO2CH3:
-# CO2CH3_92 = [
-# "C       -0.3040310000     -0.3455310000      6.3801460000",
-# "O        0.2421910000      0.6697130000      7.0866270000",
-# "O       -0.9497930000     -1.1812320000      6.9448760000",
-# "C        0.6548560000      1.8999850000      6.5015750000",
-# "H        0.5710570000      2.6581930000      7.2871520000",
-# "H        0.0047540000      2.2058210000      5.6649300000",
-# "H        1.6917900000      1.8565640000      6.1422620000"
-# ]
-# # Place H92
-# H_92 = [
-# "H92        -0.2117800000     -0.4729400000      5.9405600000"
-# ]
-# H92 = [H_92, CH3_92, CF3_92, OCH3_92, CO2CH3_92]
-# H92_names = ["H", "CH3", "CF3", "OCH3", "CO2CH3"]
-#
-# ########################################################################
-#
-# ###################################
-# # Place Substituent at position 5 (H94)
-# ###################################
-#
-# # MUST REMOVE H94 (not H93!)
-#
-# # Place CH3:
-# CH3_94 = [
-# "C       -2.6158330000     -0.3031400000      4.8210060000",
-# "H       -3.0460330624     -1.2739031262      4.6889194409",
-# "H       -3.2640832164      0.4349702427      4.3969011468",
-# "H       -2.4887386380     -0.1084434882      5.8654390000"
-# ]
-# # Place CF3:
-# CF3_94 = [
-# "C       -2.5820440000     -0.3350760000      4.8732360000",
-# "F       -3.6427160000     -0.2937500000      4.0689090000",
-# "F       -2.6904030000      0.6776680000      5.7425820000",
-# "F       -2.6639850000     -1.4633960000      5.5910920000"
-# ]
-# # Place OCH3:
-# OCH3_94 = [
-# "O       -2.4383650000     -0.4613620000      4.7867740000",
-# "C       -3.6043990000     -0.7668860000      4.0651390000",
-# "H       -4.3222680000     -1.1700650000      4.7904060000",
-# "H       -3.4324150000     -1.5363870000      3.2930830000",
-# "H       -4.0443470000      0.1240660000      3.5826780000"
-# ]
-# # Place CO2CH3:
-# CO2CH3_94 = [
-# "C       -2.5988610000     -0.2742020000      4.8277710000",
-# "O       -3.4376200000      0.7555730000      4.6006930000",
-# "O       -2.9269730000     -1.1600490000      5.5620920000",
-# "C       -3.0309130000      2.0379230000      4.1389490000",
-# "H       -3.9281340000      2.5065490000      3.7168150000",
-# "H       -2.2480480000      2.0051120000      3.3739750000",
-# "H       -2.6577920000      2.6368930000      4.9836590000"
-# ]
-# H_94 = [
-# "H94        -2.2540500000     -0.3862200000      4.5605000000"
-# ]
-#
-# H94 = [H_94, CH3_94, CF3_94, OCH3_94, CO2CH3_94]
-# H94_names = ["H", "CH3", "CF3", "OCH3", "CO2CH3"]
-
-
-###########################################################################################
-#Done with initialization
-
-###########################################################################################
-
-from dict import H91, H92, H94, H91_names, H92_names, H94_names
+#Andy Morgan
+#8/18/2020
+from dict import H91, H92, H94, H91_names, H92_names, H94_names #load all possible substituents
 import numpy as np
 from copy import deepcopy
-import os
-with open('example.inp', 'rt') as fd:
-    mylines = []
-    for myline in fd:
-        mylines.append(myline.rstrip('\n'))
-#Change the number of processes you want ot use for each
-nprocs = "8"
-mylines[3] = mylines[3][:-2]+nprocs
+import os, shutil, IPython
 
-#comment out line 117 (H91), zero-based-indexing!
-mylines[117] = '#'+mylines[117]
-
-#comment out line 118 (H92), zero-based-indexing!
-mylines[118] = '#'+mylines[118]
-
-#comment out line 120 (H94), zero-based-indexing!
-mylines[120] = '#'+mylines[120]
-
+############
+#function(s)
+###########
+'''shity way to insert strings into file'''
 def insert_sub(array, sub):
     for i in range(len(sub)):
         array.append(sub[i])
     return array
 
-for replace_91 in range(len(H91)):
-    cpy91 = deepcopy(mylines)
-    cpy91 = insert_sub(cpy91, H91[replace_91])
-    for replace_92 in range(len(H92)):
-        cpy92 = deepcopy(cpy91)
-        cpy92 = insert_sub(cpy92, H92[replace_92])
-        for replace_94 in range(len(H94)):
-            cpy94 = deepcopy(cpy92)
-            cpy94 = insert_sub(cpy94, H94[replace_94])
-            cpy94.extend(["*"]) #add the star at the end
-            with open('input_files/'+H91_names[replace_91]+'_'+H92_names[replace_92]+'_'+H94_names[replace_94]+'.inp', 'w') as f:
-                for item in cpy94:
-                    f.write("%s\n" % item)
+############
+#Main Script
+############
+#clear and then generate directory if it does not exist
+if os.path.exists('gen_input_files'):
+    shutil.rmtree('gen_input_files')
+for retry in range(100): #windows sucks and sometimes this is necessary
+    try:
+        os.makedirs('gen_input_files')
+        break
+    except:
+        None
 
-inner = input('All files created in inpute_files folder... Press Enter to quit...')
+############
+#User input and filter substituents
+############
+in91 = input ('Location 3: "H", "F", "CH3", "CF3", "OCH3", "CO2CH3", "COCH3", "CONMe2", "Beg":   ')
+in92 = input ('Location 4: "H", "F", "CH3", "CF3", "OCH3", "CO2CH3", "COCH3", "CONMe2", "Beg":   ')
+in94 = input ('Location 5: "H", "F", "CH3", "CF3", "OCH3", "CO2CH3", "COCH3", "CONMe2", "Beg":   ')
+
+#Filter out all the options, according to what Dr. Pabst wants
+idx91 = H91_names.index(in91)
+idx92 = H91_names.index(in92)
+idx94 = H91_names.index(in94)
+
+H91 = [list(H91[idx91])]
+H91_names = [H91_names[idx91]]
+H92 = [list(H92[idx92])]
+H92_names = [H92_names[idx92]]
+H94 = [list(H94[idx94])]
+H94_names = [H94_names[idx94]]
+
+##########
+#Generate files for each main group
+##########
+for eleme in ["Co", "Free", "Rad"]:
+    '''open the reference files and use the main headers'''
+    with open('refs/'+eleme+'.inp', 'rt') as fd:
+        mylines = []
+        for myline in fd:
+            mylines.append(myline.rstrip('\n'))
+    #Change the number of processes you want ot use for each
+    nprocs = "12"
+    mylines[3] = mylines[3][:-2]+nprocs
+
+    '''main loop for generating the files'''
+    for replace_91 in range(len(H91)):
+        cpy91 = deepcopy(mylines)
+        cpy91 = insert_sub(cpy91, H91[replace_91])
+        for replace_92 in range(len(H92)):
+            cpy92 = deepcopy(cpy91)
+            cpy92 = insert_sub(cpy92, H92[replace_92])
+            for replace_94 in range(len(H94)):
+                cpy94 = deepcopy(cpy92)
+                cpy94 = insert_sub(cpy94, H94[replace_94])
+                cpy94.extend(["*"]) #add the star at the end
+                with open('gen_input_files/'+eleme+'_'+H91_names[replace_91]+'_'+H92_names[replace_92]+'_'+H94_names[replace_94]+'.inp', 'w') as f:
+                    for item in cpy94:
+                        f.write("%s\n" % item)
+
+dnt_fuk_w_me = input('All files created in gen_input_files folder... Press Enter to quit...')
